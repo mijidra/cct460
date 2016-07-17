1c3589f87bf48ad141e69a766a9eaa2ec74e46f3
mijidra <theivenm@sheridanc.on.ca>
2016-07-17 18:32:56 -0400

    ue
    Had a spelling mistake.
    enque vs. enqueue
    Lesson learned. Hopefully.

--- mtheivendra1/functions.php
+++ mtheivendra1/functions.php
@@ -1,6 +1,5 @@
 <?php
 function child_enqueue_scripts(){
-	wp_enque_style( 'parent-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent-css', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts' );
-?>
