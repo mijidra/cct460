c9ba8b1857c4c9a99fb6521e225d92614764522d
mijidra <theivenm@sheridanc.on.ca>
2016-07-17 16:18:02 -0400

    /
    I forgot to put a /
    This tiiiiny mistake caused an error in my code.
    It happens.

--- mtheivendra1/functions.php
+++ mtheivendra1/functions.php
@@ -1,5 +1,5 @@
 <?php
 function child_enqueue_scripts(){
-	wp_enque_style('parent-css', get_template_directory_uri() . 'style.css');
+	wp_enque_style('parent-css', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts','child_enqueue_scripts');
